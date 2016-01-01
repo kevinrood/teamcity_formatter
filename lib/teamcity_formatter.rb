9b8a3ef56ef0b3d69846435f8e1531a646ab603f
Kevin Rood <kevin.rood@avispl.com>
2016-01-01 14:54:33 -0500

    Add comment making clear why we're using a different path name than the gem's name

--- lib/teamcity_formatter.rb
+++ lib/teamcity_formatter.rb
@@ -1,1 +1,4 @@
+# Using 'team_city_formatter' as path to support cucumber's ability to load formatters based on constant name.
+# See Cucumber::Constantize#constantize
+
 require_relative 'team_city_formatter/formatter'
